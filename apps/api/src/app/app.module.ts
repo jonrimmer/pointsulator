b607d639c76d7812d2ea43c6fba20fb71a536624
Jon Rimmer <jon.rimmer@gmail.com>
2019-06-11 20:40:28 +0100

    Remove template app code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PlayersModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [TypeOrmModule.forRoot(), PlayersModule]
 })
 export class AppModule {}

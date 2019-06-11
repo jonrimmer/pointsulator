ab0acb8d92ba6d8df7f757e6a2d63216f1c132bf
Jon Rimmer <jon.rimmer@gmail.com>
2019-06-11 20:10:06 +0100

    Scaffold NgRx

--- apps/pointsulator/src/app/app.module.ts
+++ apps/pointsulator/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NoopAnimationsModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
   providers: [],
   bootstrap: [AppComponent]
 })
